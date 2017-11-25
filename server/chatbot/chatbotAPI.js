c5864e32625880a1018628cf22ae4b8308ed65b1
An Thien Tran <tranthienan.90@gmail.com>
2017-11-25 21:30:02 +0000

    Refactor code related to chatbot api

---
+++ server/chatbot/chatbotAPI.js
@@ -0,0 +1,57 @@
+const request = require('request');
+
+const chatbotApi = {
+    handleMessage: function(senderPSID, receivedMessage) {
+        let response;
+        if (receivedMessage.nlp && receivedMessage.nlp.entities) {
+            
+            const entities = receivedMessage.nlp.entities;
+            console.log(entities);
+            
+            if (entities.intent.length > 0) {
+                const intent = entities.intent[0];
+                if (intent.value === "talk_action" && intent.confidence > 0.6)
+                    // Create the payload for a basic text message
+                    response = {
+                        "text": `Ok, set up the chat with ${entities.person[0].value}`
+                    };
+            }
+        }
+        else {
+            if (receivedMessage.text) {
+                response = {
+                    "text": `You said: "${receivedMessage.text}"`
+                };
+            }
+    
+        }
+    
+        // Sends the response message
+        callSendAPI(senderPSID, response);
+    },
+
+    callSendAPI: function(senderPSID, response) {
+        let requestBody = {
+            "recipient": {
+                "id": senderPSID
+            },
+            "message": response
+        };
+    
+        // Send the HTTP request to the Messenger Platform
+        request({
+            "uri": "https://graph.facebook.com/v2.6/me/messages",
+            "qs": { "access_token": process.env.PAGE_ACCESS_TOKEN },
+            "method": "POST",
+            "json": requestBody
+        }, (err, res, body) => {
+            if (!err) {
+                console.log('message sent!')
+            } else {
+                console.error("Unable to send message:" + err);
+            }
+        });
+    }
+}
+
+module.exports = chatbotApi;
--- server/server.js
+++ server/server.js
@@ -4,7 +4,7 @@ const bodyParser = require('body-parser');
 
 const { mongoose } = require('./db/mongoose');
 const { Patient } = require('./models/patient');
-
+const chatbotAPI = require('./chatbot/chatbotAPI');
 const app = express();
 
 const port = process.env.PORT || 3000;
@@ -59,7 +59,7 @@ app.post('/webhook', (req, res) => {
             // Check if the event is a message or postback and
             // pass the event to the appropriate handler function
             if (webhook_event.message) {
-                handleMessage(sender_psid, webhook_event.message);
+                chatbotAPI.handleMessage(sender_psid, webhook_event.message);
             } else if (webhook_event.postback) {
                 handlePostback(sender_psid, webhook_event.postback);
             }
@@ -74,64 +74,10 @@ app.post('/webhook', (req, res) => {
     }
 });
 
-function handleMessage(sender_psid, received_message) {
-    let response;
-    if (received_message.nlp && received_message.nlp.entities) {
-        
-        const entities = received_message.nlp.entities;
-        console.log(entities);
-        
-        if (entities.intent.length > 0) {
-            const intent = entities.intent[0];
-            if (intent.value === "talk_action" && intent.confidence > 0.6)
-                // Create the payload for a basic text message
-                response = {
-                    "text": `Ok, set up the chat with ${entities.person[0].value}`
-                };
-        }
-    }
-    else {
-        if (received_message.text) {
-
-            // Create the payload for a basic text message
-            response = {
-                "text": `You said: "${received_message.text}"`
-            };
-        }
-
-    }
-
-    // Sends the response message
-    callSendAPI(sender_psid, response);
-}
 
 // Handles messaging_postbacks events
 function handlePostback(sender_psid, received_postback) {
 
-}
-
-// Sends response messages via the Send API
-function callSendAPI(sender_psid, response) {
-    let request_body = {
-        "recipient": {
-            "id": sender_psid
-        },
-        "message": response
-    };
-
-    // Send the HTTP request to the Messenger Platform
-    request({
-        "uri": "https://graph.facebook.com/v2.6/me/messages",
-        "qs": { "access_token": process.env.PAGE_ACCESS_TOKEN },
-        "method": "POST",
-        "json": request_body
-    }, (err, res, body) => {
-        if (!err) {
-            console.log('message sent!')
-        } else {
-            console.error("Unable to send message:" + err);
-        }
-    });
 }
 
 app.listen(port, () => {
