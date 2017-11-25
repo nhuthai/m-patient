0331731aeb937bd5930e519544266a790bd5c0c4
An Thien Tran <tranthienan.90@gmail.com>
2017-11-25 18:11:28 +0000

    Add GET /webhook

--- server/server.js
+++ server/server.js
@@ -8,6 +8,32 @@ const port = process.env.PORT || 3000;
 
 app.use(bodyParser.json());
 
+app.get('/webhook', (req, res) => {
+
+    const VERIFY_TOKEN = process.env.VERIFY_TOKEN;
+
+    // Parse params from the webhook verification request
+    let mode = req.query['hub.mode'];
+    let token = req.query['hub.verify_token'];
+    let challenge = req.query['hub.challenge'];
+
+    // Check if a token and mode were sent
+    if (mode && token) {
+
+        // Check the mode and token sent are correct
+        if (mode === 'subscribe' && token === VERIFY_TOKEN) {
+
+            // Respond with 200 OK and challenge token from the request
+            console.log('WEBHOOK_VERIFIED');
+            res.status(200).send(challenge);
+
+        } else {
+            // Responds with '403 Forbidden' if verify tokens do not match
+            res.sendStatus(403);
+        }
+    }
+});
+
 app.listen(port, () => {
     console.log(`Server is up on port ${port}`);
 });
