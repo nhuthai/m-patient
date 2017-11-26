5332598a1b5cf307f8bd6c41a24736cc667a2c25
An Thien Tran <tranthienan.90@gmail.com>
2017-11-26 00:06:47 +0000

    Debug

--- server/chatbot/chatbotAPI.js
+++ server/chatbot/chatbotAPI.js
@@ -120,6 +120,8 @@ const chatbotApi = {
                             }
                         }
                     };
+
+                    console.log(response);
                 });
         }
 
