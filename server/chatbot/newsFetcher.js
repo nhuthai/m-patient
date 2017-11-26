5a52e090d9527393138e6a789ae8a6b192214ac2
An Thien Tran <tranthienan.90@gmail.com>
2017-11-26 09:23:17 +0000

    Update newsFetcher to get news based on the disease

--- server/chatbot/newsFetcher.js
+++ server/chatbot/newsFetcher.js
@@ -1,8 +1,12 @@
 const axios = require('axios');
 
+const { Patient } = require('./../models/patient');
+
 const newsFetcher = {
-    fetch: function () {
-        const address = "https://api.cognitive.microsoft.com/bing/v7.0/search?q=lung+cancer+stories&count=5&mkt=\"en-us\"";
+    fetch: function (user) {
+        const disease = encodeURIComponent(user.disease);
+
+        const address = `https://api.cognitive.microsoft.com/bing/v7.0/search?q=${disease}+stories&count=5&mkt=\"en-us\"`;
 
         var config = {
             headers: { "Ocp-Apim-Subscription-Key": process.env.MICROSOFT_KEY }
