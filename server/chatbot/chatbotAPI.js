e926a1ac48a1b2cf19d8a9c58ce1d5a936c344d7
An Thien Tran <tranthienan.90@gmail.com>
2017-11-26 00:47:01 +0000

    Debug

--- server/chatbot/chatbotAPI.js
+++ server/chatbot/chatbotAPI.js
@@ -110,7 +110,6 @@ const chatbotApi = {
                         return;
                     }
 
-                    console.log('Patients', patients);
                     response = {
                         "attachment": {
                             "type": "template",
@@ -121,19 +120,8 @@ const chatbotApi = {
                                      "title":"Welcome to Peter\'s Hats",
                                      "image_url":"https://petersfancybrownhats.com/company_image.png",
                                      "subtitle":"We\'ve got the right hat for everyone.",
-                                     "default_action": {
-                                       "type": "web_url",
-                                       "url": "https://peterssendreceiveapp.ngrok.io/view?item=103",
-                                       "messenger_extensions": true,
-                                       "webview_height_ratio": "tall",
-                                       "fallback_url": "https://peterssendreceiveapp.ngrok.io/"
-                                     },
                                      "buttons":[
-                                       {
-                                         "type":"web_url",
-                                         "url":"https://petersfancybrownhats.com",
-                                         "title":"View Website"
-                                       },{
+                                        {
                                          "type":"postback",
                                          "title":"Start Chatting",
                                          "payload":"DEVELOPER_DEFINED_PAYLOAD"
