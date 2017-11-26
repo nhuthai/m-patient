294414967a54d6dfe5154de98771c59b0f9433c2
An Thien Tran <tranthienan.90@gmail.com>
2017-11-26 00:34:19 +0000

    Debug

--- server/chatbot/chatbotAPI.js
+++ server/chatbot/chatbotAPI.js
@@ -4,7 +4,6 @@ const _ = require('lodash');
 
 const chatbotApi = {
     handleMessage: function (senderPSID, receivedMessage) {
-
         Patient.findByFbId(senderPSID)
             .then((doc) => {
                 if (!doc) {
@@ -117,7 +116,31 @@ const chatbotApi = {
                             "type": "template",
                             "payload": {
                                 "template_type": "generic",
-                                "elements": this.buildListReponse(patients)
+                                "elements":[
+                                    {
+                                     "title":"Welcome to Peter\'s Hats",
+                                     "image_url":"https://petersfancybrownhats.com/company_image.png",
+                                     "subtitle":"We\'ve got the right hat for everyone.",
+                                     "default_action": {
+                                       "type": "web_url",
+                                       "url": "https://peterssendreceiveapp.ngrok.io/view?item=103",
+                                       "messenger_extensions": true,
+                                       "webview_height_ratio": "tall",
+                                       "fallback_url": "https://peterssendreceiveapp.ngrok.io/"
+                                     },
+                                     "buttons":[
+                                       {
+                                         "type":"web_url",
+                                         "url":"https://petersfancybrownhats.com",
+                                         "title":"View Website"
+                                       },{
+                                         "type":"postback",
+                                         "title":"Start Chatting",
+                                         "payload":"DEVELOPER_DEFINED_PAYLOAD"
+                                       }              
+                                     ]      
+                                   }
+                                 ]
                             }
                         }
                     };
