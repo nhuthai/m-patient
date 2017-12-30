652d0c1c5b1f671cddf3bc2fcbc426f2ec615484
Hai Huynh <Nhut.H.Huynh@student.fh-Kiel.de>
2017-12-30 13:15:22 +0100

    Refactor serialization.js

--- server/ml/serialization.js
+++ server/ml/serialization.js
@@ -1,16 +1,27 @@
-function serialize(arr){
+/**
+ * Implements and stores Bayeasian Network.
+ * Compares Symptoms of two patients.
+ * @Author: Nhut Hai Huynh
+ */
+
+/**
+ * Convert array of objects to array of JSON objects.
+ * @return {array} - converted JSON objects array.
+ * @param {array} arr - Array of Objects.
+ * and Disease.
+ */
+function serialize(arr) {
     var text = '{';
-    for (i = 0; i < arr.length; i ++){
-        if(i == 0){
+    for (i = 0; i < arr.length; i ++) {
+        if (i == 0) {
             text += '"q' + (i+1) + '" : ' + arr[i].toString();
-        }
-        else{
+        } else {
             text += ',"q' + (i+1) + '" : ' + arr[i].toString();
         }
     }
     text += '}';
-    json_obj = JSON.parse(text);
-    return [json_obj];
+    var jsonObj = JSON.parse(text);
+    return [jsonObj];
 }
 
 module.exports = { serialize };
